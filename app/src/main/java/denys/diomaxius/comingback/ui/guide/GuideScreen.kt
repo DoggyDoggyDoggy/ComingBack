d5b81dd7fd890cc28007d37dbaf2a46ae58be49e
Doggy <pedan.denys@gmail.com>
2024-10-16 23:04:22 +1300

    Gate button color change

--- app/src/main/java/denys/diomaxius/comingback/ui/guide/GuideScreen.kt
+++ app/src/main/java/denys/diomaxius/comingback/ui/guide/GuideScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -77,14 +78,20 @@ fun GuideScreen(
         ) {
             Button(
                 modifier = Modifier.padding(horizontal = 20.dp),
-                onClick = { guideViewModel.previousGuide() }
+                onClick = { guideViewModel.previousGuide() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF324763)
+                )
             ) {
                 Text(text = "Prev Gate")
             }
 
             Button(
                 modifier = Modifier.padding(horizontal = 20.dp),
-                onClick = { guideViewModel.nextGuide() }
+                onClick = { guideViewModel.nextGuide() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF324763)
+                )
             ) {
                 Text(text = "Next Gate")
             }
